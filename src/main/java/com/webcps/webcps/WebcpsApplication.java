c2c3895d8a66ca4c3c0fdcd66634c8a2eaaf7677
Tarun Singh <tarun.singh@pb.com>
2018-02-06 13:42:44 +0530

    webcps project

---
+++ src/main/java/com/webcps/webcps/WebcpsApplication.java
@@ -0,0 +1,19 @@
+package com.webcps.webcps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WebcpsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebcpsApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcpsApplication.class, args);
+	}
+}
